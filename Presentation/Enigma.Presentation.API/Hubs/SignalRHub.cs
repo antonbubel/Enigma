ad68c6317163786da4838d3a99cbfd3d5306a2d8
Anton Bubel <a.bubel@itransition.com>
2019-04-07 17:12:18 +0300

    Configure startup

---
+++ Presentation/Enigma.Presentation.API/Hubs/SignalRHub.cs
@@ -0,0 +1,9 @@
+﻿namespace Enigma.Presentation.API.Hubs
+{
+    using Microsoft.AspNetCore.SignalR;
+    
+    public class SignalRHub : Hub
+    {
+
+    }
+}
--- Presentation/Enigma.Presentation.API/Startup.cs
+++ Presentation/Enigma.Presentation.API/Startup.cs
@@ -1,20 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
-
-namespace Enigma.Presentation.API
+﻿namespace Enigma.Presentation.API
 {
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.DependencyInjection;
+
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.SignalR;
+    using Microsoft.AspNetCore.Cors.Infrastructure;
+
+    using Hubs;
+
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
+            services.AddSignalR();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,11 +28,27 @@ namespace Enigma.Presentation.API
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
+            else
             {
-                await context.Response.WriteAsync("Hello World!");
-            });
+                app.UseHsts();
+            }
+
+            app.UseCors(ConfigureCorsPolicy);
+            app.UseSignalR(ConfigureHubRouteBuilder);
+            app.UseMvc();
+        }
+
+        private void ConfigureCorsPolicy(CorsPolicyBuilder corsPolicyBuilder)
+        {
+            corsPolicyBuilder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+        }
+
+        private void ConfigureHubRouteBuilder(HubRouteBuilder hubRouteBuilder)
+        {
+            hubRouteBuilder.MapHub<SignalRHub>("/enigma-signalr-hub");
         }
     }
 }
